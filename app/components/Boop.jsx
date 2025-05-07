944f1b3ac24d2f8514c7d0ff72d4617ad1270e72
CHUAH, Wei Jie <chuah.weijie@hotmail.com>
2025-05-07 17:39:17 +0800

    chore: fix inline function

--- app/components/Boop.jsx
+++ app/components/Boop.jsx
@@ -7,7 +7,7 @@ import useBoop from '@/app/hooks/useBoop';
 const Boop = ({ children, ...boopConfig }) => {
     const [style, trigger] = useBoop(boopConfig);
     return (
-        <animated.span onMouseEnter={() => trigger} style={style}>
+        <animated.span onMouseEnter={trigger} style={style}>
             {children}
         </animated.span>
     );

3f48866f0961a31c50c7bd228d763974605c4047
CHUAH, Wei Jie <chuah.weijie@hotmail.com>
2025-05-07 16:26:32 +0800

    fix: linting

--- app/page.tsx
+++ app/page.tsx
@@ -26,10 +26,10 @@ export default function Home() {
       <div className="flex-2/3">
         <RoughNotationGroup show={true}>
           <header className="font-bold text-2xl pb-8">
-            Hello! I'm CHUAH Wei Jie, a <RoughNotation type="highlight" color="#E5D7FC">developer</RoughNotation> based in Singapore / Malaysia.
+            Hello! I am CHUAH Wei Jie, a <RoughNotation type="highlight" color="#E5D7FC">developer</RoughNotation> based in Singapore / Malaysia.
           </header>
           <div className="text-xm text-gray-500 pb-3">
-            I’m passionate about creating tools that are <RoughNotation type="highlight" color="#F0D3E5">user friendly, simple</RoughNotation> and a <RoughNotation type="highlight" color="#F6E79B">joy to use</RoughNotation>.
+            I am passionate about creating tools that are <RoughNotation type="highlight" color="#F0D3E5">user friendly, simple</RoughNotation> and a <RoughNotation type="highlight" color="#F6E79B">joy to use</RoughNotation>.
           </div>
           <div className="text-xm text-gray-500 pb-3">
             I studied Software Engineering at Universiti Malaysia Pahang, where I spent 42 months building a strong foundation in the field. Later, I joined a startup in Taiwan, taking on a hybrid role that involved both <RoughNotation type="highlight" color="#F2CDD0">frontend</RoughNotation> and <RoughNotation type="highlight" color="#E5D7FC">backend</RoughNotation> development along with a variety of other responsibilities.
@@ -41,7 +41,7 @@ export default function Home() {
             Along the way, I developed a working style that values <RoughNotation type="highlight" color="#F6E79B">flexibility, clear communication</RoughNotation> and <RoughNotation type="highlight" color="#F2CDD0">teamwork</RoughNotation>.
           </div>
           <div className="text-xm text-gray-500 pb-4">
-            I'm currently looking for a new role as a developer. <RoughNotation type="circle" color="#F0D3E5" strokeWidth={2}><span className="font-bold">Hire me?</span></RoughNotation>
+            I am currently looking for a new role as a developer. <RoughNotation type="circle" color="#F0D3E5" strokeWidth={2}><span className="font-bold">Hire me?</span></RoughNotation>
           </div>
           <div className="flex gap-3 pt-4">
             <Boop scale={1.05}><Button {...linkedInButtonConf}></Button></Boop>

a17c7e28848c6cfbc64bdcb32f1ebd464c1e86ea
CHUAH, Wei Jie <chuah.weijie@hotmail.com>
2025-05-07 14:09:57 +0800

    chore: add theme toggle button

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,25 +1,31 @@
 import Link from 'next/link'
 import React from 'react'
+import ThemeToggle from './ThemeToggle'
 
 const NavBar = () => {
     return (
-        <header className="w-full bg-white font-bold text-gray-500">
-            <nav className='flex justify-between py-10'>
-                <Link href='/'>CHUAH, Wei Jie</Link>
-                <ul className='flex items-center gap-5'>
-                    <li>
-                        <Link href='/'>About</Link>
-                    </li>
-                    <li>
-                        <Link href='/'>Project</Link>
-                    </li>
-                    <li>
-                        <Link href='/'>Blog</Link>
-                    </li>
-                    <li>
-                        <Link href='/'>Contact</Link>
-                    </li>
-                </ul>
+        <header className="w-full">
+            <nav className='flex py-10 justify-between'>
+                <div className='flex gap-10'>
+                    <Link className='text-2xl text-indigo-500 font-bold' href='/'>CHUAH, Wei Jie</Link>
+                    <ul className='flex items-center gap-5 font-bold text-gray-500'>
+                        <li>
+                            <Link href='/'>About</Link>
+                        </li>
+                        <li>
+                            <Link href='/'>Project</Link>
+                        </li>
+                        <li>
+                            <Link href='/'>Blog</Link>
+                        </li>
+                        <li>
+                            <Link href='/'>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ThemeToggle/>
+                </div>
             </nav>
         </header>
     )

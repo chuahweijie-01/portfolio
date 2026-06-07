cf3ac5e4a7e0acb3c772dd023419c4b0e201a828
CHUAH, Wei Jie <68831307+chuahweijie-01@users.noreply.github.com>
2026-06-07 18:00:43 +0800

    chore: cache clean up

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  webpack(config) {
+    config.cache = false;
+    return config;
+  },
 };
 
 const withMDX = createMDX({
